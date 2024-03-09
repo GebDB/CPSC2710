459f3b5fc2e32cc05b5384af36dc27c4684fd2b9
Geb <becirevicdaniel@gmail.com>
2024-03-09 01:01:18 -0600

    Added methods in SeatReservation class

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,8 +1,67 @@
 package module1;
+import java.time.LocalDate;
 
 public class SeatReservation {
     private String flightDesignator;
     private java.time.LocalDate flightDate;
     private String firstName;
     private String lastName;
+
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+    public void setFlightDesignator(String fd) {
+        flightDesignator = fd;
+    }
+    public java.time.LocalDate getFlightDate() {
+        return flightDate;
+    }
+    public void setFlightDate(java.time.LocalDate date) {
+        flightDate = date;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String fn) {
+        firstName = fn;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String ln) {
+        lastName = ln;
+    }
+    public String toString() {
+        String result = "SeatReservation{flightDesignator=";
+
+        if (flightDesignator == null) {
+            String fd = "null";
+            result = result + fd;
+        }
+        else {
+            result = result + flightDesignator;
+        }
+
+        String ld = flightDate.toString();
+        result = result + ",flightDate=" + ld + ",firstName=";
+        if (firstName == null) {
+            String fn = "null";
+            result = result + fn;
+        }
+        else {
+            result = result + firstName;
+        }
+
+        result = result + ",lastName=";
+        if (lastName == null) {
+            String ln = "null";
+            result = result + ln;
+        }
+        else {
+            result = result + lastName;
+        }
+        result = result + "}.";
+
+        return result;
+    }
 }
