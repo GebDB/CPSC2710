ab165daf8f9102cb4d0d20e0520775d6f4d40c17
Geb <becirevicdaniel@gmail.com>
2024-03-09 00:41:08 -0600

    Added SeatReservation Class

---
+++ module1/SeatReservation.java
@@ -0,0 +1,8 @@
+package module1;
+
+public class SeatReservation {
+    private String flightDesignator;
+    private java.time.LocalDate flightDate;
+    private String firstName;
+    private String lastName;
+}
