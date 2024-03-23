47e9e8ee3b249c24241e8f538879a235c5a75a39
Geb <becirevicdaniel@gmail.com>
2024-03-23 08:56:03 -0500

    Corrected the URL functionality and added updateFields(Airport airport) to execute after the webViewAction method.

--- module3/src/main/java/edu/au/cpsc/module3/AirportController.java
+++ module3/src/main/java/edu/au/cpsc/module3/AirportController.java
@@ -1,19 +1,25 @@
 package edu.au.cpsc.module3;
-import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebView;
-import javafx.fxml.FXML;
 import javafx.scene.web.WebEngine;
-
 import java.io.IOException;
-import java.lang.reflect.Array;
+import java.text.DecimalFormat;
 import java.util.List;
-import java.util.Objects;
 
 public class AirportController {
+    public TextField municipalityTextfield;
+    @FXML
+    public TextField regionTextfield;
+    @FXML
+    public TextField countryTextfield;
+    @FXML
+    public TextField elevationTextfield;
+    @FXML
+    public TextField nameTextfield;
+    @FXML
+    public TextField typeTextfield;
     @FXML
     private WebView webView;
     @FXML
@@ -23,14 +29,16 @@ public class AirportController {
     @FXML
     private TextField localCodeTextfield;
     @FXML
-    private Button searchButton;
+    private Button searchButton; // Declared for functionality.
 
+    // Updates webView and calls updateFields().
     @FXML
     protected void webViewAction() throws IOException {
         String searchTerm;
         Airport airport = new Airport();
         boolean found = false;
 
+        // Check if the fields are empty and find the correct airport object.
         if (!identTextfield.getText().isEmpty()) {
             searchTerm = identTextfield.getText();
             List<Airport> airports = Airport.readAll();
@@ -42,14 +50,14 @@ public class AirportController {
                 }
             }
             if (!found) {
-                return;
+                return; // End method if the ident cannot be found.
             }
         }
         else if (!iataCodeTextfield.getText().isEmpty()) {
             searchTerm = iataCodeTextfield.getText();
             List<Airport> airports = Airport.readAll();
             for (Airport tempAirport : airports) {
-                System.out.println(tempAirport.getIataCode());
+
                 if (tempAirport.getIataCode().equalsIgnoreCase(searchTerm)) {
                     airport = tempAirport;
                     found = true;
@@ -57,7 +65,7 @@ public class AirportController {
                 }
             }
             if (!found) {
-                return;
+                return; // End method if the iata code cannot be found.
             }
         }
         else if (!localCodeTextfield.getText().isEmpty()) {
@@ -71,22 +79,34 @@ public class AirportController {
                 }
             }
             if (!found) {
-                return;
+                return; // End method if the local code cannot be found.
             }
         }
         else {
-            return;
+            return; // End method if all textfields are empty.
         }
-        System.out.println(airport.getIataCode());
-        try {
-            Double[] coordinates = airport.getCoordinates();
-            String url = "https://www.windy.com/?" + coordinates[0].toString() + "," + coordinates[1].toString();
-            WebEngine webEngine = webView.getEngine();
-            webEngine.load(url);
-        }
-        catch (Exception e) {
 
-            System.out.println(e);
-        }
+        //Set the url and load the url into the webEngine. Then call updateFields().
+        Double[] coordinates = airport.getCoordinates();
+        DecimalFormat df = new DecimalFormat("#.###");
+        String coordinate1 = df.format(coordinates[0]);
+        String coordinate2 = df.format(coordinates[1]);
+        String url = "https://www.windy.com/?" + coordinate2 + "," + coordinate1 + ",12" ;
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load(url);
+        updateFields(airport);
     }
+    //Updates the UI fields after webViewAction is successfully called and executed.
+    private void updateFields(Airport airport) {
+        municipalityTextfield.setText(airport.getMunicipality());
+        regionTextfield.setText(airport.getRegion());
+        countryTextfield.setText(airport.getCountry());
+        elevationTextfield.setText(airport.getElevationft().toString());
+        nameTextfield.setText(airport.getName());
+        typeTextfield.setText(airport.getType());
+        identTextfield.setText(airport.getIdent());
+        iataCodeTextfield.setText(airport.getIataCode());
+        localCodeTextfield.setText(airport.getLocalCode());
+    }
+
 }
