7b73072e67b8f15ae582770a76f97c85b87cc139
Geb <becirevicdaniel@gmail.com>
2024-03-22 20:12:59 -0500

    UI textfields and button added.

--- module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
+++ module3/src/main/java/edu/au/cpsc/module3/AirportApplication.java
@@ -9,22 +9,18 @@ import java.io.IOException;
 import java.util.List;
 
 public class AirportApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AirportApplication.class.getResource("hello-view.fxml"));
+        List<Airport> x = Airport.readAll();
+        FXMLLoader fxmlLoader = new FXMLLoader(AirportApplication.class.getResource("airportapp-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) throws IOException {
-        // Quick test for readAll method.
-        List<Airport> x = Airport.readAll();
-        for (Airport airport : x) {
-            Double[] a = airport.getCoordinates();
-            System.out.println(a[0] + "..." + a[1]);
-        }
+    public static void main(String[] args) {
 
         launch();
     }
