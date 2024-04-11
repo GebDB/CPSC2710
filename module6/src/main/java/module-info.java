1df259c8538505b459901fc46f0cba09751e1b41
Geb <becirevicdaniel@gmail.com>
2024-04-11 01:02:40 -0500

    setup

---
+++ module6/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module edu.au.cpsc.part1 {
+        requires javafx.controls;
+        requires javafx.fxml;
+
+
+        opens edu.au.cpsc.part1 to javafx.fxml;
+        exports edu.au.cpsc.part1;
+}
+
